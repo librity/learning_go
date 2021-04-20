fbafb673671d53b6beac275b85ab904abf80b2b4
Luis Geniole <luis.geniole@gmail.com>
2021-04-20 11:07:53 -0300

    Finish structs demo.

---
+++ nc_theory/structs.go
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.go                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/20 10:54:12 by lpaulo-m          #+#    #+#             */
+/*   Updated: 2021/04/20 11:07:33 by lpaulo-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// go run structs.go
+// go build structs.go && ./structs
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("=== Structs ===")
+
+	structs_demo()
+}
+
+type person struct {
+	name           string
+	age            int
+	favorite_foods []string
+}
+
+func structs_demo() {
+	booze := []string{"jack", "jameson", "kirsch", "smirnoff"}
+	lior := person{"lior", 42, booze}
+
+	fmt.Println(lior)
+	fmt.Println(lior.name)
+	fmt.Println(lior.age)
+	fmt.Println(lior.favorite_foods)
+	fmt.Println("---")
+
+	mass_media := []string{
+		"garmonbozia", "garmonbozia", "garmonbozia", "garmonbozia",
+		"garmonbozia", "garmonbozia", "garmonbozia", "garmonbozia",
+		"garmonbozia", "garmonbozia", "garmonbozia", "garmonbozia",
+		"garmonbozia", "garmonbozia", "garmonbozia", "garmonbozia",
+		"garmonbozia", "garmonbozia", "garmonbozia", "garmonbozia",
+		"garmonbozia", "garmonbozia", "garmonbozia", "garmonbozia",
+		"garmonbozia", "garmonbozia", "garmonbozia", "garmonbozia",
+		"garmonbozia", "garmonbozia", "garmonbozia"}
+	lior_after_rehab := person{name: "lior", age: 42, favorite_foods: mass_media}
+
+	fmt.Println(lior_after_rehab)
+	fmt.Println(lior_after_rehab.name)
+	fmt.Println(lior_after_rehab.age)
+	fmt.Println(lior_after_rehab.favorite_foods)
+	fmt.Println("---")
+}
